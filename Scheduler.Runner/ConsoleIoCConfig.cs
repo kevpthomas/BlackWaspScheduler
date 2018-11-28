2429d9183b8926d4fe2d92ce237fbc55b08050c5
kevin.thomas <kevin.thomas@equinox.co.nz>
2018-11-28 15:47:04 +1300

    Initial architectural configuration following https://github.com/ardalis/CleanArchitecture

---
+++ Scheduler.Runner/ConsoleIoCConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
+using IoC;
+
+namespace Scheduler.Runner
+{
+    public class ConsoleIoCConfig : IoCConfig
+    {
+        private readonly Lazy<Assembly[]> _iocAssemblies = new Lazy<Assembly[]>(() =>
+        {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.Combine(path, "Scheduler.Infrastructure.dll"));
+            return AppDomain.CurrentDomain.GetAssemblies();
+        });
+
+        protected override Assembly[] IocAssemblies => _iocAssemblies.Value;
+
+        public static ConsoleIoCConfig Instance { get; } = new ConsoleIoCConfig();
+    }
+}
---
+++ Scheduler.Runner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Scheduler.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleIoCConfig.Instance.Register();
+ 
+            Console.Clear();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
